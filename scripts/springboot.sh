98851fdb656561eef5426fa170c3ca90448f6c99
lety <tkrhktyd@gmail.com>
2022-12-03 20:22:49 +0900

    액션 추가

---
+++ scripts/springboot.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+REPO=/home/ec2-user/actions-runner/_work/java-todo
+PROJECT=java-todo
+
+CURR_PID=$(pgrep -f ${PROJECT}.*.jar)
+if [ -n "$CURR_PID" ]; then
+  echo "already running java process ---> kill -15 $CURR_PID"
+  kill -15 "$CURR_PID"
+  sleep 8
+fi
+
+# extract the largest size jar
+JAR=$(ls -S $REPO/$PROJECT/build/libs/*.jar | head -n 1)
+
+echo "application start: $JAR"
+echo "log path: $REPO/$PROJECT.log"
+
+# When nohup errors, output errors to log file
+nohup java -jar "$JAR" > $REPO/$PROJECT.log 2>&1 &
